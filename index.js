20903cfd4e12fef1c95a7808979137b9937fd026
Yash282003 <114577553+Yash282003@users.noreply.github.com>
2023-12-14 09:02:47 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,16 +1,21 @@
-const productRoutes =require('./routes/productRoutes.js');
+const productRoutes = require("./routes/productRoutes.js");
 const mongoose = require("mongoose");
 const cors = require("cors");
 const express = require("express");
 
 const users = require("./models/Users");
-const {getProductForReview, createProductReview, updateProductReview,getUserReviews} = require('./controllers/productController');
+const {
+  getProductForReview,
+  createProductReview,
+  updateProductReview,
+  getUserReviews,
+} = require("./controllers/productController");
 
 const app = express();
 app.use(express.json());
 app.use(cors());
 app.use(express.urlencoded({ extended: true }));
-app.use('/',productRoutes)
+app.use("/", productRoutes);
 const DB =
   "mongodb+srv://mailyashika28:yashmongo@cluster0.hv29e74.mongodb.net/?retryWrites=true&w=majority";
 mongoose
@@ -24,18 +29,17 @@ app.post("/register", async (req, resp) => {
   try {
     let user = new users(req.body);
     let result = await user.save();
-    result=result.toObject();
+    result = result.toObject();
     delete result.password;
-    resp.send(result)
+    resp.send(result);
   } catch (error) {
     console.error("Error saving user:", error);
-    resp.status(500).send("Error saving user");
+    resp.status(500).send("Error saving user", error);
   }
 });
 app.get("/hello", async (req, resp) => {
   try {
-   
-    resp.send("hello ")
+    resp.send("hello");
   } catch (error) {
     console.error("Error saving user:", error);
     resp.status(500).send("Error saving user");
@@ -43,19 +47,21 @@ app.get("/hello", async (req, resp) => {
 });
 app.post("/login", async (req, resp) => {
   try {
-    const user = await users.findOne({ email: req.body.email, password: req.body.password }).select("-password");
+    const user = await users
+      .findOne({ email: req.body.email, password: req.body.password })
+      .select("-password");
     if (user) {
       resp.send(user);
     } else {
-      resp.send({ result: 'no user found' });
+      resp.send({ result: "no user found" });
     }
   } catch (error) {
     console.error("Error finding user:", error);
-    resp.status(500).send({ result: 'error finding user' });
+    resp.status(500).send({ result: "error finding user" });
   }
 });
-app.get('/products',getProductForReview);
-app.post('/:id/reviews',createProductReview)
-app.patch('/:id/reviews/update', updateProductReview)
-app.get('/:userId/reviews',getUserReviews)
+app.get("/products", getProductForReview);
+app.post("/:id/reviews", createProductReview);
+app.patch("/:id/reviews/update", updateProductReview);
+app.get("/:userId/reviews", getUserReviews);
 app.listen(5000);
